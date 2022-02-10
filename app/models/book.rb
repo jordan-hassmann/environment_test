57105bbad525d9d2feafa42fc5ec6df2367b6398
jhassmann <15932+jhassmann@users.noreply.github.tamu.edu>
2022-02-10 11:40:53 -0600

    Book collection Week 3 Assignment

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,4 @@
 class Book < ApplicationRecord
-   validates :title, presence: true
-   validates :author, presence: true
-   validates :price, presence: true
-   validates :published_date, presence: true
+   validates :title, :author, :price, :published_date, presence: true
+   has_many :books
 end
